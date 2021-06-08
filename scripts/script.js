0e2dce4f1a22d27fba65c308946a7a122c93994c
“Your <adrianaarroyom@gmail.com>
2021-06-07 21:15:47 -0600

    galeria

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+function vermas (id)
+    if(id== "mas"){
+        document.getElementById("desplegar").style.display= "block";
+        document.getElementById("mas").style.display= "none";
+    }
+    else {
+        document.getElementById("desplegar").style.display= "none";
+        document.getElementById("mas").style.display= "inline";
+    }
+
+    function vermenos (id)
+    if(id== "menos"){
+        document.getElementById("desplegar").style.display= "block";
+        document.getElementById("menos").style.display= "none";
+    }
+    else {
+        document.getElementById("desplegar").style.display= "none";
+        document.getElementById("menos").style.display= "inline";
+    }
+
